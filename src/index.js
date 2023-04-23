88b893a05bdf5a47e89c142fa32b48150e51e8ea
NoBody <113786996+amppmann@users.noreply.github.com>
2023-04-24 00:54:19 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// const express = require("express");
+
+// const app = express();
+
+// console.log("Hello");
+
+
+// const fs = require('fs');
+
+// fs.copyFileSync("file1.txt","file2.txt");
+
+
+
+const superheroes = require("superheroes");
+
+
+// var mySuperName = superheroes.random();
+// console.log(mySuperName);
+
+
+const superVillains = require("supervillains");
+
+
+var myVillains = superVillains.random();
+console.log(myVillains);
